17040285db08929932abb1d3aedb49da9615a1d4
Cat73 <root@cat73.org>
2025-03-02 19:21:04 +0800

    :sparkles: 测试一下发邮件相关

--- src/http/controller/dev.ts
+++ src/http/controller/dev.ts
@@ -1,5 +1,8 @@
 import { Context } from 'koa'
 import { ResultObj, useResult } from '../../util/result.js'
+import { useKnex } from '../../db/knex.js'
+import config from '../../config/index.js'
+import dayjs from 'dayjs'
 
 /**
  * 内部测试接口 - 前端不应该对接此处的任何接口
@@ -11,7 +14,13 @@ class DevController {
    * GET /api/dev/dev
    */
   async dev(ctx: Context): Promise<ResultObj<any>> {
-    return useResult().success()
+    // 测试查询角色清单
+    const res1 = await useKnex().raw(`SELECT charac_no, charac_name FROM taiwan_cain.charac_info WHERE m_id = 18000000`)
+
+    // 36 = 服务器喇叭
+    const res2 = await useKnex().raw(`INSERT INTO taiwan_cain_2nd.postal (occ_time, send_charac_name, receive_charac_no, item_id, add_info, gold) VALUES ('${dayjs().format('YYYY-MM-DD HH:mm:ss')}', 'LinuxDo', 1, 36, 1, 100)`)
+
+    return useResult().success({ res1, res2 })
   }
 }
 
