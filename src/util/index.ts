f89d231e3a75fbb2a6a552e49721d309ce827bb7
Cat73 <root@cat73.org>
2025-03-03 07:21:49 +0800

    :sparkles: 正确发放限时物品

--- src/util/index.ts
+++ src/util/index.ts
@@ -30,7 +30,8 @@ export async function sendReward(dnfId: number, characId: number | null, reward:
   // 发放物品
   if (reward.items) {
     for (const item of reward.items) {
-      await useKnex().raw(`INSERT INTO taiwan_cain_2nd.postal (occ_time, send_charac_name, receive_charac_no, item_id, add_info, endurance) VALUES ('${dayjs().format('YYYY-MM-DD HH:mm:ss')}', '${config.dnfMailSender}', ${characId}, ${item.id}, ${item.count}, 6822)`)
+      const endTime = Math.ceil((Date.now() - 1151683200000) / 86400000) + 30
+      await useKnex().raw(`INSERT INTO taiwan_cain_2nd.postal (occ_time, send_charac_name, receive_charac_no, item_id, add_info, endurance) VALUES ('${dayjs().format('YYYY-MM-DD HH:mm:ss')}', '${config.dnfMailSender}', ${characId}, ${item.id}, ${item.count}, ${endTime})`)
     }
   }
 }
