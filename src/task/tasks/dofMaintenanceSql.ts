f12488169277d4a25e698390bba2f3460ce61669
Cat73 <root@cat73.org>
2025-03-05 08:06:16 +0800

    :fire: 用 SQL 加公会服务器好像不太会刷新，需要重启才能让数据正常，所以先关掉这个功能，手动邀请吧

--- src/task/tasks/dofMaintenanceSql.ts
+++ src/task/tasks/dofMaintenanceSql.ts
@@ -7,13 +7,13 @@ export const dofMaintenanceSql = async () => {
   // 清理封号信息
   await useKnex().raw(`DELETE FROM d_taiwan.member_punish_info`)
   // 自动加入公会
-  await useKnex().raw(`UPDATE taiwan_cain.charac_info SET guild_id = 1 WHERE guild_id = 0`)
-  await useKnex().raw(`
-    INSERT INTO d_guild.guild_member (guild_id, m_id, server_id, charac_no, charac_name, memo, grade, job, grow_type, lev, age, born_year, sex, apply_time, member_time, member_flag, bbs_cnt, last_visit_time, secede_type, secede_time, member_point, member_point_prev, last_play_time, nick_name)
-    SELECT
-      1, taiwan_cain.charac_info.m_id, 3, taiwan_cain.charac_info.charac_no, taiwan_cain.charac_info.charac_name, '', 3, taiwan_cain.charac_info.job,
-      taiwan_cain.charac_info.grow_type, taiwan_cain.charac_info.lev, 0, '00', '-', NOW(), NOW(), 1, 0, NOW(), 0, NOW(), 0, 0, NOW(), ''
-    FROM taiwan_cain.charac_info
-    WHERE (SELECT COUNT(*) FROM d_guild.guild_member WHERE d_guild.guild_member.charac_no = taiwan_cain.charac_info.charac_no) <= 0
-  `)
+  // await useKnex().raw(`UPDATE taiwan_cain.charac_info SET guild_id = 1 WHERE guild_id = 0`)
+  // await useKnex().raw(`
+  //   INSERT INTO d_guild.guild_member (guild_id, m_id, server_id, charac_no, charac_name, memo, grade, job, grow_type, lev, age, born_year, sex, apply_time, member_time, member_flag, bbs_cnt, last_visit_time, secede_type, secede_time, member_point, member_point_prev, last_play_time, nick_name)
+  //   SELECT
+  //     1, taiwan_cain.charac_info.m_id, 3, taiwan_cain.charac_info.charac_no, taiwan_cain.charac_info.charac_name, '', 3, taiwan_cain.charac_info.job,
+  //     taiwan_cain.charac_info.grow_type, taiwan_cain.charac_info.lev, 0, '00', '-', NOW(), NOW(), 1, 0, NOW(), 0, NOW(), 0, 0, NOW(), ''
+  //   FROM taiwan_cain.charac_info
+  //   WHERE (SELECT COUNT(*) FROM d_guild.guild_member WHERE d_guild.guild_member.charac_no = taiwan_cain.charac_info.charac_no) <= 0
+  // `)
 }
